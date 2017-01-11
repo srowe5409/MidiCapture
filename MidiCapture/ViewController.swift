1c776ac54f80ac7f79462f4c856b12919fc959e9
srowe5409 <srowe5409@gmail.com>
2017-01-11 00:41:48 -0600

    Initial Commit

---
+++ MidiCapture/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MidiCapture
+//
+//  Created by Stephen Rowe on 1/11/17.
+//  Copyright © 2017 Stephen Rowe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
